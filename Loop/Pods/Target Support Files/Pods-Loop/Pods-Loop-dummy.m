ccfdd695514885c87257e1ea8ccab2ea7cd65685
Dallin Burton <dallin.burton@icloud.com>
2020-10-17 00:35:53 -0600

    Remove Pod stuff
    Remove pod stuff

--- Loop/Pods/Target Support Files/Pods-Loop/Pods-Loop-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Loop : NSObject
-@end
-@implementation PodsDummy_Pods_Loop
-@end
--- Loop/Pods/Target Support Files/Pods-LoopTests/Pods-LoopTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LoopTests : NSObject
-@end
-@implementation PodsDummy_Pods_LoopTests
-@end
