cb1b1e1e06337574911d4fc26bf34792fd3ebb91
lissaramos <lissaramos@users.noreply.github.com>
2020-10-17 13:39:21 -0700

    Changed Base Card

--- Loop/Loop/Components/BaseCard.swift
+++ Loop/Loop/Components/BaseCard.swift
@@ -9,27 +9,34 @@ import SwiftUI
 
 struct BaseCard: View{
     var Title:String
-    var TextBody:String
+    var BodyLabel:String
     var body: some View {
 		ZStack {
 
 			
-			RoundedRectangle(cornerRadius: 25, style: .continuous)
-				.fill(Color.red)
-				.frame(width: /*@START_MENU_TOKEN@*/115/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-			RoundedRectangle(cornerRadius: 25, style: .continuous)
+            RoundedRectangle(cornerRadius: 10.0, style: .continuous)
 				.fill(Color.blue)
-				.frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-			Text("")
-		}
-		
+				.frame(width: 300, height:150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .shadow(color:.blue, radius: 10)
+                
+            VStack{
+               Text(Title)
+                .font(.title)
+                .fontWeight(.black)
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill(Color.white)
+                    .frame(width: 275, height: 80, alignment: .center)
+            
+            }.overlay(Text(BodyLabel))
+            Spacer()
+        }
     }
 }
 
 
 struct BaseCard_Previews: PreviewProvider {
     static var previews: some View {
-        BaseCard(Title:"Assignments", TextBody: "peepeepoopoo")
+        BaseCard(Title:"Assignments", BodyLabel:"peepeepoopoo")
     }
 }
 
--- Loop/Loop/ContentView.swift
+++ Loop/Loop/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-		BaseCard(Title: "hello", TextBody: "world")
+        BaseCard(Title: "hello", BodyLabel: "world")
     }
 }
 
