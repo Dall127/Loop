7afc5cf8de0e297f480f7d363e99174d9c94aa99
Dallin Burton <dallin.burton@icloud.com>
2020-10-16 20:08:34 -0600

    intial commit
    Made initial commit

---
+++ Loop/Loop/LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopApp.swift
+//  Loop
+//
+//  Created by Dallin Burton on 10/16/20.
+//
+
+import SwiftUI
+
+@main
+struct LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
