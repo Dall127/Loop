e6b83e49136ab04d0f891c949ad6672166029499
Guillermo Ramos <memo33101@gmail.com>
2020-10-17 01:38:15 -0600

    Fixed firebase configuration at load

--- Loop/Loop/LoopApp.swift
+++ Loop/Loop/LoopApp.swift
@@ -6,13 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct LoopApp: App {
-	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("setting up firebase")
+        FirebaseApp.configure()
+        print("finished setting up firebase")
+        return true
     }
 }
