478947e9699bd64cd25ec538137dd0aa88063856
Guillermo Ramos <memo33101@gmail.com>
2020-10-18 04:21:25 -0600

    Revert "Merge branch 'SecondaryCreate' into student-assignment-view"
    This reverts commit f90f360f395200711f79437618a979efcf662d14, reversing
    changes made to 68e126509bf06f1f4ea79da44258ca72ceb7dc76.

--- Loop/Loop/Components/Models.swift
+++ Loop/Loop/Components/Models.swift
@@ -12,7 +12,6 @@ import FirebaseFirestore
 
 class StudentViewModel: ObservableObject {
 	var UserID = ""
-	var isStudent = false
 	@Published var student = Student()
 	private var db = Firestore.firestore()
 	
@@ -35,10 +34,7 @@ class StudentViewModel: ObservableObject {
 							
 						}
 					}
-					self.isStudent = true
-					return
 				}
-				
 				
 			
 
@@ -60,7 +56,6 @@ struct Student: Identifiable {
 
 class TeacherViewModel: ObservableObject {
 	var UserID = ""
-	var isTeacher = false
 	@Published var teacher = Teacher()
 	private var db = Firestore.firestore()
 	
@@ -83,10 +78,7 @@ class TeacherViewModel: ObservableObject {
 							self.teacher.classes.append(docID!)
 							
 						}
-					
 					}
-					self.isTeacher = true
-					return
 				}
 				
 				
@@ -103,46 +95,5 @@ struct Teacher: Identifiable {
 	var id = UUID()
 	var userID = ""
 	var classes = Array<String>()
-	
-}
-
-class Assignemnts: ObservableObject {
-	var problems = Array<String>()
-	var className = ""
-	private var db = Firestore.firestore()
-	
-	func fetchData() -> Void {
-		db.collection("classes").collectionID("assignments").addSnapshotListener { (querySnapshot, error) in
-			guard let documents = querySnapshot?.documents else {
-				print("No documents") // the printing is done here
-				return
-			}
-			for  queryDocumentSnapshot in documents {
-				let data = queryDocumentSnapshot.data()
-				self.aClass.name = data["name"] as? String ?? ""
-				let assignmentsArray = data["problems"] as? Array ?? []
-				for i in assignmentsArray {
-					let docID = (i as? DocumentReference)?.documentID
-					if(docID != nil) {
-						self.aClass.problems.append(docID!)
-						
-					}
-				}
-				
-				
-				
-			}
-			
-		}
-	}
-	func setClassName(className: String) {
-		self.className = className
-	}
-}
-
-struct Assignment: Identifiable {
-	var id = UUID()
-	var name = ""
-	var problems =  Array<String>()
 	
 }
