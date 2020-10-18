4f8774a09fc23c3e378a2c68524eee9d4e00c23d
Guillermo Ramos <memo33101@gmail.com>
2020-10-17 23:12:58 -0600

    Implemented the increment functionality with a transaction

--- Loop/Loop/Views/Assignment/Problem/StudentProblemView.swift
+++ Loop/Loop/Views/Assignment/Problem/StudentProblemView.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
+
 struct StudentProblemView: View {
+    //Setup
     let db = Firestore.firestore()
+
+    
     var body: some View {
         //Query name from database
         GroupBox(label: Text("Problem 1")) {
@@ -28,18 +33,49 @@ struct StudentProblemView: View {
     }
     //These two submit functions will probably live inside ProblemView
     func submitFeelingGood(){
-        db.collection("classes").document("MATH1220").collection("assignments").document("assign02").setData([
-            //1 is a placeholder - we want to add 1 to the current number stored.
-            "feeling-good" : 1
-            
-        ], merge: true)
+        incrementTransaction(fieldName: "feeling-good")
     }
     func submitFeelingBad(){
-        db.collection("classes").document("MATH1220").collection("assignments").document("assign02").setData([
-            //1 is a placeholder - we want to add 1 to the current number stored.
-            "feeling-bad" : 1
-        ], merge: true)
+        incrementTransaction(fieldName: "feeling-bad")
     }
+    
+    func incrementTransaction(fieldName: String){
+        let assignmentRef = db.collection("classes").document("MATH1220").collection("assignments").document("assign02")
+        db.runTransaction({ (transaction, errorPointer) -> Any? in
+            let sfDocument: DocumentSnapshot
+            do {
+                try sfDocument = transaction.getDocument(assignmentRef)
+            } catch let fetchError as NSError {
+                errorPointer?.pointee = fetchError
+                return nil
+            }
+
+            guard let oldCount = sfDocument.data()?[fieldName] as? Int else {
+                let error = NSError(
+                    domain: "AppErrorDomain",
+                    code: -1,
+                    userInfo: [
+                        NSLocalizedDescriptionKey: "Unable to retrieve population from snapshot \(sfDocument)"
+                    ]
+                )
+                errorPointer?.pointee = error
+                return nil
+            }
+
+            // Note: this could be done without a transaction
+            //       by updating the count using FieldValue.increment()
+            transaction.updateData([fieldName: oldCount + 1], forDocument: assignmentRef)
+            return nil
+        }) { (object, error) in
+            if let error = error {
+                print("Transaction failed: \(error)")
+            } else {
+                print("Transaction successfully committed!")
+            }
+        }
+
+    }
+
 }
 
 struct StudentProblemView_Previews: PreviewProvider {
