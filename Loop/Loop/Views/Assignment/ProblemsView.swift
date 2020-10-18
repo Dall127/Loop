8553686c7a6bf96cb62c969992c7a79af8d226fd
Guillermo Ramos <memo33101@gmail.com>
2020-10-18 15:22:29 -0600

    Adjusted ProblemsView to not depend on AssignmentView and deleted AssignmentView

--- Loop/Loop/Views/Assignment/ProblemsView.swift
+++ Loop/Loop/Views/Assignment/ProblemsView.swift
@@ -10,38 +10,52 @@ import FirebaseFirestore
 
 struct ProblemsView: View {
     let db = Firestore.firestore()
-    
     let assignmentPath: String
-    init(assignmentPath: String) {
+    let assignmentName: String
+    
+    init(assignmentName: String, assignmentPath: String) {
+        self.assignmentName = assignmentName
         self.assignmentPath = assignmentPath
     }
+    
     var body: some View {
-        Spacer().onAppear(perform: {
-            db.document(assignmentPath).collection("problems").getDocuments() { (querySnapshot, err) in
-                if let err = err {
-                    print("Error getting documents: \(err)")
-                } else {
-                    var i = 0
-                    for document in querySnapshot!.documents {
-                        paths[i] = document.reference.path
-                        i+=1
+        VStack{
+            Text(assignmentName)
+                .font(.title)
+                .bold()
+                .multilineTextAlignment(.leading)
+            Spacer().onAppear(perform: {fetchDocumentPaths()})
+            List{
+                VStack{
+                    ForEach(0..<paths.count, id: \.self) { i in
+                        //StudentProblemView(documentPath: paths[i])
+                        Text(paths[i])
                     }
                 }
             }
-        })
-        List{
-            ForEach(0..<paths.count, id: \.self) { i in
-                StudentProblemView(documentPath: paths[i])
+        }
+    }
+        
+    func fetchDocumentPaths(){
+        db.document(assignmentPath).collection("problems").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                var i = 0
+                for document in querySnapshot!.documents {
+                    paths[i] = document.reference.path
+                    i+=1
+                }
             }
         }
     }
-    
 }
 private var paths = [String]()
 
+
 struct ProblemsView_Previews: PreviewProvider {
     static var previews: some View {
-        ProblemsView(assignmentPath: "classes/MATH1220/assignments/assign02")
+        ProblemsView(assignmentName: "Assignment 2", assignmentPath: "classes/MATH1220/assignments/assign02")
     }
 }
 
